c931b0cc37a149e31851d1e6d20e53d3d3ff9a1d
ksham <kshamala700@gmail.com>
2024-06-29 20:03:32 +0530

    pattern

--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -1,15 +1,18 @@
-//object test {
-  //def main(args:Array[String]):Unit= {
-    //var num=1
+object test {
+  def main(args: Array[String]): Unit = {
+    var num = 1
 
-    //for(i<-5 to 1 by -1){
-      //for(j<-1 to i) {
+    for (i <- 5 to 1 by -1) {
+      for (j <- 1 to i) {
 
-        //print(j)
+        print(j)
+      }
+      println()
+    }
 
-      //}
-      //println()
+  }
+}
 
 
 
-  //}
+
