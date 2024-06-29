682bbfa91e8e6bc2957e7f13a741823ba5ec4e12
ksham <kshamala700@gmail.com>
2024-06-29 15:42:39 +0530

    Demo program

---
+++ src/main/scala/test.scala
@@ -0,0 +1,14 @@
+object test {
+  def main(args:Array[String]):Unit= {
+    var num=1
+    for(i<-5 to 1 by -1){
+      for(j<-1 to i) {
+
+        print(j)
+
+      }
+      println()
+    }
+
+  }
+  }
